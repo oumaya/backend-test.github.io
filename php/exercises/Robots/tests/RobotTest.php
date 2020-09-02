61c42726c3e5c0e99061645a926f4db0a9b689d0
Oumaya <abdelkhalek.oumaya@gmail.com>
2020-09-02 11:33:18 +0200

    RobotTest.php

---
+++ php/exercises/Robots/tests/RobotTest.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class RobotTest extends TestCase
+{
+    public function testValidType(): void
+    {
+        $robot = new Robot('walking');
+        $this->assertEquals(
+            'WL',
+            $robot->type
+        );
+    }
+
+  public function testInvalidType(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $robot = new Robot('invalid');
+    }
+
+    public function testCanFly(): void
+    {
+        $robot = new Robot('flying');
+        $this->assertEquals(
+            'robot fly',
+            $robot ->fly()
+        );
+    }
+    public function testCantFly(): void
+    {
+        $robot = new Robot('walking');
+        $this->assertNotEquals(
+            'robot fly',
+            $robot ->fly()
+        );
+    }
+    public function testCanWalking(): void
+    {
+        $robot = new Robot('walking');
+        $this->assertEquals(
+            'robot walking',
+            $robot ->fly()
+        );
+    }
+    public function testCantWalking(): void
+    {
+        $robot = new Robot('flying');
+        $this->assertNotEquals(
+            'robot walking',
+            $robot ->fly()
+        );
+    }
+    
+}
