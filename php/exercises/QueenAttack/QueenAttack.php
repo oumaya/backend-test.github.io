c861dd3f43e1495e923ede5fef217e5d309aacf1
Oumaya <abdelkhalek.oumaya@gmail.com>
2020-09-02 11:30:38 +0200

    reponse queenAttack oumaya.php

--- php/exercises/QueenAttack/QueenAttack.php
+++ php/exercises/QueenAttack/QueenAttack.php
@@ -9,6 +9,11 @@ class QueenAttack
     public function placeQueen(int $i, int $j): bool
     {
 
+		if ( (in_array($i, range(1,8))) && (in_array($j, range(1,8)))) {
+   				 return true;
+		}else{
+		throw new Exception('invalide value must be between [0,8]');
+		}
     }
 
     /**
@@ -18,6 +23,27 @@ class QueenAttack
      */
     public function canAttack(array $white, array $black): bool
     {
+		
+		if(($this->placeQueen($white[0], $white[1]))&&
+		   ($this->placeQueen($black[0], $black[1]))){
+			
+			if ($white[0] == $black[0]) 
+			{   return true;}
 
+			if ($white[1] == $black[1]) 
+			{ return true;	}
+
+		    if (abs($white[0] - $black[0]) == abs($white[1] - $black[1]))
+	        {return true; }
+			
+		}
+        return false;
     }
+	
+	
+
 }
+
+$a = new QueenAttack();
+
+echo $a-> canAttack([2,3], [5,6]);
